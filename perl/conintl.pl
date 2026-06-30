ba7686ec783efa723fee72d0c8882b90a29ac9e0
AGNI <aescobedo@agmail.com>
2026-06-29 20:50:58 -0600

    chore: actualizar .gitignore y conintl.pl

--- perl/conintl.pl
+++ perl/conintl.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 # perl/conintl.pl
-# Uso: plackup conintl.pl  -> http://localhost:5000/?n=10
+# Ejecutar: plackup conintl.pl -p 5003
+# URL: http://localhost:5003/?n=10
 # Convierte un número a letras en español usando Lingua::ES::Numeros
 
 use strict;
@@ -9,16 +10,20 @@ use Plack::Request;
 use Lingua::ES::Numeros;
 
 my $app = sub {
-    my $env     = shift;
-    my $req     = Plack::Request->new($env);
-    my $n       = $req->param('n') // 0;
-
-    my $conv    = Lingua::ES::Numeros->new(GENERO => 'M');
-    my $texto   = $conv->cardinal($n);
-
+    my $env = shift;
+    my $req = Plack::Request->new($env);
+    
+    my $n = $req->param('n') // 0;
+    
+    my $conv = Lingua::ES::Numeros->new(GENERO => 'M');
+    my $texto = $conv->cardinal($n);
+    
     return [
         200,
         ['Content-Type' => 'text/plain; charset=UTF-8'],
         [ $texto ]
     ];
 };
+
+# IMPORTANTE: Devolver la referencia de código
+$app;
